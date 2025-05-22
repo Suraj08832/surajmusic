1438a7385f586bcec0de8781e969c655ff6dbcae
Your Name <your-email@example.com>
2025-05-22 23:40:06 +0530

    Initial commit: Added music bot code with Railway deployment configuration

---
+++ surajmusic/__init__.py
@@ -0,0 +1,30 @@
+from surajmusic.core.bot import Hotty
+from surajmusic.core.dir import dirr
+from surajmusic.core.git import git
+from surajmusic.core.userbot import Userbot
+from surajmusic.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Hotty()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "BRANDED_KUDI_BOT"  # connect music api key "Dont change it"
